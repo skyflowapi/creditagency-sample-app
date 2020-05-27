e8d6fa54b6a4bc0056ecd6b18159e0bf2af624d3
vasanth <vasanth.kumar@zemosolabs.com>
2020-05-27 20:17:32 +0530

    add build files

--- webpack/paths.js
+++ webpack/paths.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   root: path.resolve(__dirname, "../"),
-  outputPath: path.resolve(__dirname, "../", "build"),
+  outputPath: path.resolve(__dirname, "../", "docs"),
   entryPath: path.resolve(__dirname, "../", "src/index.js"),
   templatePath: path.resolve(__dirname, "../", "src/template.html"),
   imagesFolder: "images",
