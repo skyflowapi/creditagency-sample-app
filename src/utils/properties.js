93dbbd02ef1ea9c59efb3c80dd90776b21c6a08b
Achyuth Chandra Annakula <achyuth.chandra@zemosolabs.com>
2020-06-18 16:15:55 +0530

    added github ribbon

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,7 +1,7 @@
 export default {
   GITHUB_REPO_URL:
     process.env.GITHUB_REPO_URL ||
-    "https://github.com/skyflowtech/react-sample-app",
+    "https://github.com/skyflowtech/creditagency-sample-app",
   SKYFLOW_APP_ID:
     process.env.SKYFLOW_APP_ID || "bfa831cab06e11ea81abc6d2cc16794f",
 };
