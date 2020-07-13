8ce9cda464d8ce73a40be370f324b19707a49c7a
achyuthztpl <achyuth.chandra@zemosolabs.com>
2020-07-13 13:11:26 +0530

    removed workflow url from clientSDK and updated the default analyst screen data

--- src/services/Skyflow.js
+++ src/services/Skyflow.js
@@ -5,15 +5,9 @@ import properties from "../utils/properties";
 const Skyflow = (props) => {
   const [skyflow] = React.useState(
     new window.Skyflow({
-      // username: properties.username,
-      // password: properties.password,
       orgId: properties.orgId,
-      // orgAppId: "c0e915a9904011ea95ba2e321592fd49",
-      // orgAppSecret: "MpdB0NTs8KT3oBG7rhSTwB5kJ1c=",
       vaultId: properties.vaultId,
-      // notebookId: properties.notebookId,
       appId: properties.appId,
-      workflowURL: properties.workflowURL,
     })
   );
   const [elements] = React.useState(skyflow.elements({}));
